aadb3039a6ff5b569f65683ed91f8ba1941ffeb6
Nikolay Nechaev <nikolay_nechaev@mail.ru>
2018-12-28 17:03:55 +0300

    Now intarface is VERY cool

--- interface.py
+++ interface.py
@@ -3,6 +3,7 @@ from mpl_toolkits.basemap import Basemap
 from backend import get_reply
 from backend import get_city_information as get_city_info
 from backend import get_statistic as get_stats
+import readline
 
 
 def start():
@@ -30,29 +31,36 @@ def game():
 communication with player """
 	last_city = None
 	while True:
-		city = input("Enter a city: ")
-		if(city == '-'):
+		try:
+			print("Enter a city:")
+			city = ''
+			while(city == ''):
+				city = input()
+			if(city == '-'):
+				break
+			reply = get_reply(last_city, city)
+			if not reply['ok']:
+				if(reply['error'] == "is used"):
+					print("We've already used this city", end='')
+				elif(reply['error'] == "mistake"):
+					print("You have to enter a city that starts with letter {}".format(last_city[-1].upper()), end='')
+				else:
+					print("You've entered a city I don't know", end='')
+				print("... Try again, please")
+				continue
+			info = get_city_info(city)
+			draw_plot(info['coordinates'][1], info['coordinates'][0], "{} ({})".format(city, info['population']), 'Your city')
+			#print("You've entered a city with coordinates {} with population {}".format(info['coordinates'], info['population'])) # Instead of this there'll be a plot drawing
+			if reply['city'] is None:
+				break
+			info = get_city_info(reply['city'])
+			print("I chose {}".format(reply['city']))
+			draw_plot(info['coordinates'][1], info['coordinates'][0], "{} ({})".format(reply['city'], info['population']), "Computer's city")
+			#print("I chosed a city with coordinates {} with population {}".format(info['coordinates'], info['population'])) # Instead of this there'll be a plot drawing
+			last_city = reply['city']
+		except:
+			print('I GOT AN EXCEPTION! I LET UP!')
 			break
-		reply = get_reply(last_city, city)
-		if not reply['ok']:
-			if(reply['error'] == "is used"):
-				print("We've already used this city", end='')
-			elif(reply['error'] == "mistake"):
-				print("You have to enter a city that starts with letter {}".format(last_city[-1].upper()), end='')
-			else:
-				print("You've entered a city I don't know", end='')
-			print("... Try again, please")
-			continue
-		info = get_city_info(city)
-		draw_plot(info['coordinates'][1], info['coordinates'][0], city, 'Your city')
-		#print("You've entered a city with coordinates {} with population {}".format(info['coordinates'], info['population'])) # Instead of this there'll be a plot drawing
-		if reply['city'] is None:
-			break
-		info = get_city_info(reply['city'])
-		print("I chose {}".format(reply['city']))
-		draw_plot(info['coordinates'][1], info['coordinates'][0], reply['city'], "Computer's city")
-		#print("I chosed a city with coordinates {} with population {}".format(info['coordinates'], info['population'])) # Instead of this there'll be a plot drawing
-		last_city = reply['city']
 
 def finish():
 	print("Game is finished. Results: {}".format(get_stats()))
